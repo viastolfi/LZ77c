be1b5a2fd3fb84d13bb37836566d193ef253c891
viastolfi <vincent.astolfi12@gmail.com>
2025-08-12 11:54:16 +0200

    feat: read content from file

--- src/main.c
+++ src/main.c
@@ -1,9 +1,39 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 
 #include "encode.h"
 
-char* test_string = "aabcabcc";
+char* get_content_from_file(char* path) {
+  FILE* file;
+  long length;
+  char* content;
+
+  file = fopen(path, "rb");
+  if (file == NULL)
+  {
+    perror("Error reading file");
+    return ""; 
+  }
+
+  fseek(file, 0, SEEK_END);
+  length = ftell(file);
+  fseek(file, 0, SEEK_SET);
+  content = malloc(length + 1);
+  if (content) {
+    size_t bytes_read = fread(content, 1, length, file);
+    content[bytes_read] = '\0'; 
+  }
+  else
+  {
+    fclose(file);
+    return ""; 
+  }
+
+  fclose(file);
+
+  return content;
+}
 
 int main(int argc, char** argv)
 {
@@ -12,9 +42,15 @@ int main(int argc, char** argv)
     return 1;
   }
 
+  char* content = get_content_from_file(argv[1]);
+  if(content == "") {
+    puts("[ERROR] - no content found");
+    return 1;
+  }
+
   LZ77_encoder encoder;
-  encoder.input = test_string;
-  encoder.input_start = test_string;
+  encoder.input = content;
+  encoder.input_start = content;
   encoder.window = (LZ77e_window) {.sbl = 5, .labl = 5};
 
   encoder.output.items = NULL;
